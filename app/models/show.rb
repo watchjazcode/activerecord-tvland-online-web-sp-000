9344b9d2aa20a7a4f47e5d8637bc2242f5d52ca3
Jazmin M <jazminmerckel@gmail.com>
2019-12-12 04:56:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,13 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
+
+  def actors_list
+    names = []
+    actors.each do |actor|
+      names << actor.full_name
+    end
+    names
+  end
+
 end
