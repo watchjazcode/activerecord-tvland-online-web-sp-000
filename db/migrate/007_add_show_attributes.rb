6d2df229fa9534ce672b582712978422b212fb55
Jazmin M <jazminmerckel@gmail.com>
2019-12-12 04:47:58 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_show_attributes.rb
+++ db/migrate/007_add_show_attributes.rb
@@ -1,4 +1,4 @@
-class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
+class AddShowAttributes < ActiveRecord::Migration[5.1]
     def change
       add_column :name, :day, :season
     end
